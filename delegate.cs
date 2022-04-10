b39204ed8c3bbf5c02dd1ddd155bdf534566a1ab
shivam11m <65330985+shivam11m@users.noreply.github.com>
2022-04-11 02:38:45 +0530

    Update delegate.cs

--- delegate.cs
+++ delegate.cs
@@ -2,7 +2,10 @@ using System;
 
 //Delegates
 // signature of delegate must match the signature of the functions, the delgat points to (i.e  - void, string,int etc )
-
+/*
+A delegate is a reference to a method. Whereas objects can easily be sent as parameters into methods, constructor or whatever, 
+methods are a bit more tricky. But every once in a while you might feel the need to send a method as a parameter to another method, and that's when you'll need delegates.
+*/
 public delegate void Hellofunctiondelgate(string message);
 
 class Program
